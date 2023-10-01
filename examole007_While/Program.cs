ef4cbc7c2c2e7bf8c4bdc146bdc632f261d456ff
testingQA2 <144715118+testingQA2@users.noreply.github.com>
2023-10-01 20:58:32 +0300

    While

---
+++ examole007_While/Program.cs
@@ -0,0 +1,44 @@
+﻿//Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xv = 1, yv = 10,
+    xc = 80, yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xv, yv);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = yv;
+
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if(what == 1)
+    {
+        x = (x + xv) / 2;
+        y = (y +yv) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y +yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}
